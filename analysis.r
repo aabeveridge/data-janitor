bc36c056103c77f5a28a76bd65f785d92cda8b62
aaronbev79 <aaronbev79@gmail.com>
2015-09-16 13:06:07 -0400

    adding visuals and pushing index.html for publishing webtext

--- analysis.r
+++ analysis.r
@@ -2,10 +2,128 @@
 # Wordcloud/Tagcloud Example
 #############################
 
+# Text mining package for R
 library(tm)
+
+# Tagcloud/wordcloud package for R
 library(wordcloud)
+
+# Stemming package for R
 library(SnowballC)
 
+# Color palate package for R
+library(RColorBrewer)
+
+library(ggplot2)
+library(gridExtra)
+
+# Sets the working directory for the project
+setwd("~/data-janitor")
+
+# The Corpus() function from the TM library reads in a directory of 
+# text files with DirSource() and converts them to the corpus data 
+# type for TM. 
+docs.list <- list.files("~/data-janitor/data")
+wiki.docs <- as.vector(character(0))
+
+for (i in 1:length(docs.list)){
+    d <- read.csv(paste("~/data-janitor/data/", docs.list[i], sep=""))
+    wiki.docs <- append(wiki.docs, as.character(d$extract))
+}
+
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+png(file="wc1.png", width=650, height=650)
+wordcloud(wiki.corpus, random.order=FALSE)
+dev.off()
+rm(wiki.corpus)
+
+# Creates the wordcloud based on default settings for wordcloud() 
+# and an unscrubbed corpus 
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+png(file="wc2.png", width=650, height=650)
+wordcloud(wiki.corpus, max.words=200, scale=c(6, 1), random.order=FALSE)
+dev.off()
+rm(wiki.corpus)
+
+
+wiki.docs <- removePunctuation(wiki.docs, preserve_intra_word_dashes=TRUE)
+wiki.docs <- removeNumbers(wiki.docs)
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+wiki.docs <- tolower(wiki.docs)
+wiki.docs <- removeWords(wiki.docs, stopwords(kind="en"))
+wiki.docs <- stripWhitespace(wiki.docs)
+
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+png(file="wc3.png", width=650, height=650)
+wordcloud(wiki.corpus, max.words=200, scale=c(6, 1), random.order=FALSE)
+dev.off()
+rm(wiki.corpus)
+
+# Adding random word order to help with comparison
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+png(file="wc4.png", width=650, height=650)
+wordcloud(wiki.corpus, max.words=200, scale=c(6, 1), random.order=TRUE)
+dev.off()
+rm(wiki.corpus)
+
+# Adding colors
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+png(file="wc5.png", width=650, height=650)
+wordcloud(wiki.corpus, max.words=200, scale=c(6, 1), random.order=TRUE, colors=brewer.pal(12, "Paired"), random.color=FALSE)
+dev.off()
+rm(wiki.corpus)
+
+# Changing colors
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+png(file="wc6.png", width=650, height=650)
+wordcloud(wiki.corpus, max.words=200, scale=c(6, 1), random.order=TRUE, colors=brewer.pal(8, "Dark2"), random.color=FALSE)
+dev.off()
+rm(wiki.corpus)
+
+# Changing colors removing randomization
+wiki.corpus <- Corpus(VectorSource(wiki.docs))
+png(file="wc7.png", width=650, height=650)
+wordcloud(wiki.corpus, max.words=200, scale=c(6, 1), random.order=FALSE, colors=brewer.pal(8, "Dark2"), random.color=FALSE)
+dev.off()
+rm(wiki.corpus)
+
+dtm <- DocumentTermMatrix(wiki.corpus)
+freq <- data.frame(Total = sort(colSums(as.matrix(dtm)),
+                                          decreasing=TRUE))
+write.csv(head(freq, 50), "freq.csv")
+freq.data <- read.csv("freq.csv")
+rev(freq.data)
+freq.data <- as.data.frame(freq.data)
+word.names <- head(freq.data$X, 10)
+word.names <- rev(word.names)
+word.freq <- head(freq.data$Total, 10)
+word.freq <- rev(word.freq)
+## Open PNG device and set location to save file
+png("freq_plot.png", 600, 480)
+
+	## Set plot dimensions 
+	par(pin=c(4, 4))
+	
+	## Draw plot
+	freq.plot <- barplot(word.freq, names.arg=word.names, horiz=TRUE, 
+						 main="Word Frequencies", ylab=NULL, xlab="Frequency", 
+						 las=2, axes=FALSE)
+	
+	# Starting x-axis position for adding frequency totals to bars
+	freq.pos <- 0.65		
+	
+	## Add frequency totals to bars	
+	for(i in 1:length(word.freq)) {			
+	text(freq.pos, labels=word.freq[i], pos=4, offset=.2)
+	freq.pos <- freq.pos + 1.2
+	}
+
+	## Add bottom axis and numeric range
+	axis(1, at=seq(0, 1200, 100))
+
+## Close PDF device
+dev.off()
 
 
 
+#wordcloud(wiki.corpus, max.words=200, scale=c(6, 1), random.order=FALSE, colors=brewer.pal(12, "Paired"), random.color=FALSE)
--- article.md
+++ article.md
@@ -1,15 +1,38 @@
 #The Rhetoric of Data Janitorial Work
 
-As visual rhetoric shifts its focus to data visualization and information design, the relationship between rhetoric and statistics will need to be further explored. 
-
 List of Wikipedia Articles for Corpus:
-composition studies
-English studies
-Digital rhetoric
-tecomputers and writing
-technical communication
-writing across the curriculum
-writing about writing
-visual rhetoric
-media studies
-ecocomposition
+
+- composition studies
+- English studies
+- Digital rhetoric
+- computers and writing
+- technical communication
+- writing across the curriculum
+- writing about writing
+- visual rhetoric
+- media studies
+- ecocomposition
+
+![](./images/wc1.png)
+
+![](./images/wc2.png)
+
+![](./images/wc3.png)
+
+![](./images/wc4.png)
+
+![](./images/wc5.png)
+
+![](./images/wc6.png)
+
+![](./images/wc7.png)
+
+![](./images/freq_plot.png)
+
+<!--In "Rhetorical Numbers: A Case for Quantitative Writing in the Composition Classroom," Joanna Wolfe contends that rhetorical approaches to quantitative information and reasoning remain largely overlooked in writing studies classrooms. As Wolfe argues, "Rather than reject quantitative argument out of hand, contemporary rhetoricians need to train their students to recognize the unethical, deceptive, and misleading as well as thoughtful, instightful, and revealing applications of quantitative rhetoric" (454). While visual rhetoric and design studies have turned their attention to data visualization and infographic rhetorics, much work remains to understand how the canon of invention may be applied to data scrubbing, statistical analysis, and quantitative reasoning. -->
+
+<!--In some ways, this creates an analogue between process theories of composition and the development of quantitative arguments that lead to data visualizations. Sidney Dobrin and xxx in *Abducting Writing Studies* argues that the recent "writing about writing" trend in writing studies research allows the field to "recupperate" useful elements of "process" rhetorics without returning to long-exhausted debates. -->
+
+<!--I'm working a webtext that deals with visual rhetoric and information design. It is tentatively titled: "The Rhetoric of Data Janitorial Work," and explains how the process of scrubbing data (removing unnecessary components of data, or reorganizing data for specific rhetorical purposes) for analysis and visualization can have widely varied rhetorical effects on what the final infographics may "say" or "communicate." -->
+
+<!--I create word cloud visualizations from scratch using text data--writing and sharing all of the underlying code--and I explain how the various steps in the process effect the final visual arguments that appear in the data visualizations (word clouds). As visual rhetoric continues to confront issues related to information design and infographic rhetorics, understanding how data janitorial choices limit or expand the possibilities for visual design are a crucial part of this ongoing conversation. -->
